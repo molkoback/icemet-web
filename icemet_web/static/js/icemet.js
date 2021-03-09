026eaff06869d321a5fb639ea2f67f75061a6aea
molko <molkoback@gmail.com>
2021-03-09 09:09:29 +0200

    Improved dropdown menus

--- icemet_web/static/js/icemet.js
+++ icemet_web/static/js/icemet.js
@@ -3,3 +3,31 @@
 var error = function(msg) {
 	alert(msg);
 };
+
+var icemet_init = function(url) {
+	$(".dropdown").each(function() {
+		var dropdown = this;
+		$(dropdown).find("> button").click(function() {
+			$(".dropdown-submenu-content").hide();
+			$(dropdown).siblings().each(function() {
+				$(this).find("> .dropdown-content").hide();
+			});
+			$(dropdown).find("> .dropdown-content").toggle();
+		});
+	});
+	$(".dropdown-submenu").each(function() {
+		var dropdown = this;
+		$(dropdown).find("> button").click(function() {
+			$(dropdown).siblings().each(function() {
+				$(this).find("> .dropdown-submenu-content").hide();
+			});
+			$(dropdown).find("> .dropdown-submenu-content").toggle();
+		});
+	});
+	$(document).on("click", function closeMenu (event) {
+		if ($(".dropdown").has(event.target).length === 0) {
+			$(".dropdown-content").hide();
+			$(".dropdown-submenu-content").hide();
+		}
+	});
+};
