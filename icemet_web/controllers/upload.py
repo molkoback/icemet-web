3f7a56392c6262121729e37f69bc067724bf8806
molko <molkoback@gmail.com>
2023-01-18 14:10:20 +0200

    Use temporary files in uploads

--- icemet_web/controllers/upload.py
+++ icemet_web/controllers/upload.py
@@ -5,6 +5,7 @@ from icemet.file import File
 import flask
 
 import os
+import uuid
 
 @app.route("/api/upload/", defaults={"path": "/"}, methods=["POST"])
 @app.route("/api/upload/<path:path>", methods=["POST"])
@@ -14,14 +15,19 @@ def upload_api_route(path):
 		return api(error="Invalid parameters")
 	
 	try:
-		root = os.path.normpath(app.config["ICEMET_WATCH_PATH"])
-		path = path.strip("/")
-		path = os.path.join(root, path, files["file"].filename)
-		path = os.path.normpath(path)
-		if not path.startswith(app.config["ICEMET_WATCH_PATH"]):
+		dir = os.path.join(app.config["ICEMET_WATCH_PATH"], path.strip("/"))
+		file = files["file"].filename
+		File.frompath(file)
+		
+		tmp = os.path.join(dir, ".icemet-" + uuid.uuid4().hex + os.path.splitext(file)[1])
+		tmp = os.path.normpath(tmp)
+		dst = os.path.join(dir, file)
+		dst = os.path.normpath(dst)
+		if not dst.startswith(app.config["ICEMET_WATCH_PATH"]):
 			return api(error="Invalid path")
-		File.frompath(path)
-		files["file"].save(path)
+		
+		files["file"].save(tmp)
+		os.rename(tmp, dst)
 	except Exception as e:
 		return api(error="Invalid file")
 	return api()
